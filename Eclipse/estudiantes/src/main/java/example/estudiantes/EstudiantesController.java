80a97001a5db09124169c83b6b75b440b071a603
JorgeCengotita <jluis-cengotita1@eep-igroup.com>
2024-05-10 17:24:25 +0200

    Quitado import sin usar

--- Eclipse/estudiantes/src/main/java/example/estudiantes/EstudiantesController.java
+++ Eclipse/estudiantes/src/main/java/example/estudiantes/EstudiantesController.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
